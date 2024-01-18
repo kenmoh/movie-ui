9fa54851206fa205a191285e311cc79787c0dde7
hopemoh <hopemoh2020@gmail.com>
2024-01-18 12:31:13 +0100

    add google adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,10 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta
-          name="google-adsense-account"
-          content="ca-pub-4643996002315789"
-        ></meta>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4643996002315789"
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <body className={inter.className}>
         <Navbar />
