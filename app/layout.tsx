37543969d27e0c9fb00b934b6c03a67607774fea
hopemoh <hopemoh2020@gmail.com>
2024-01-18 12:23:22 +0100

    add google adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,33 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './globals.css'
-import Navbar from '@/components/Navbar'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Movie Review',
-  description: 'Movie Review/Rating App',
-  keywords: 'movie, review, rating, app',
-}
+  title: "Movie Review",
+  description: "Movie Review/Rating App",
+  keywords: "movie, review, rating, app",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        meta name="google-adsense-account" content="ca-pub-4643996002315789"
+      </head>
       <body className={inter.className}>
         <Navbar />
         <ToastContainer />
-        <div className='mx-auto '>
-          {children}
-        </div>
+        <div className="mx-auto ">{children}</div>
       </body>
     </html>
-  )
+  );
 }
